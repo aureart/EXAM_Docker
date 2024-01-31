53688c5c9faaf3efa827b4d113375175db661327
Ubuntu <ubuntu@ip-172-31-23-45.eu-west-1.compute.internal>
2024-01-31 14:05:19 +0000

    amelioration mais ne teste pas tous les v2

--- tests/authorization/authorization_test.py
+++ tests/authorization/authorization_test.py
@@ -1,20 +1,39 @@
 import requests
 import os
+# définition de l'adresse de l'API
+api_address = os.getenv('API_ADDRESS', 'localhost')
+# port de l'API
+api_port = os.getenv('API_PORT', 8000) #port sur lequel l'api est disponilble
+log_path = os.getenv('LOG_PATH', None)
+
 
 def run_authorization_test(api_address, api_port, log_path):
     users = {
-        "alice": ["v1", "v2"],  # Alice a accès aux deux versions
-        "bob": ["v1"]           # Bob a accès uniquement à la v1
+        "alice": {"username": "alice", 
+                  "password": "wonderland", 
+                  "versions": ["v1", "v2"]},  
+        "bob": {"username": "bob",
+                 "password": "builder",
+                   "versions": ["v1"]}
     }
 
-    for username, versions in users.items():
-        for version in versions:
+    for user, details in users.items():
+        for version in details["versions"]:
             url = f"http://{api_address}:{api_port}/{version}/sentiment"
-            response = requests.get(url, params={"username": username, "password": "correct_password", "sentence": "test"})
+            params = {
+                "username": details["username"],
+                "password": details["password"],
+                "sentence": "test authorization"
+            }
+            response = requests.get(url, params=params)
 
+            # Déterminez si le test est réussi
             status_code = response.status_code
-            expected_status = 200
+            expected_status = 200 if (user != "bob" or version != "v2") else 403
             test_status = 'SUCCESS' if status_code == expected_status else 'FAILURE'
+
+            #expected_status = 200
+            #test_status = 'SUCCESS' if status_code == expected_status else 'FAILURE'
 
             output = f'''
             ============================
@@ -22,7 +41,7 @@ def run_authorization_test(api_address, api_port, log_path):
             ============================
 
             request done at "/{version}/sentiment"
-            | username="{username}"
+            | username="{details["username"]}"
 
             expected result = {expected_status}
             actual result = {status_code}
@@ -31,14 +50,10 @@ def run_authorization_test(api_address, api_port, log_path):
             '''
 
             print(output)
-            if log_path:
-                with open(log_path, 'a') as file:
-                    file.write(output)
+        if log_path:
+            with open(log_path, 'a') as file:
+                file.write(output)
 
 
 if __name__ == "__main__":
-    api_address = os.getenv('API_ADDRESS', 'localhost')
-    api_port = os.getenv('API_PORT', 8000)
-    log_path = os.getenv('LOG_PATH', None)  # Si LOG_PATH est défini, les logs seront écrits dans ce fichier
-
     run_authorization_test(api_address, api_port, log_path)
